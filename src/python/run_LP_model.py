4a3a76a3b4bec66a8f1dcf010c978ec1c2f75ee9
Kiara Tesen <katesen@uc.cl>
2022-11-01 13:33:46 -0600

    run_prueba

--- src/python/run_LP_model.py
+++ src/python/run_LP_model.py
@@ -1,38 +1,37 @@
 import pandas as pd
 from utils import *
 
-
-#### Cargamos datos
-acciones = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Acciones")
-activaciones = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Activacion")
-clima = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Clima")
-demanda = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Demanda")
-
-acciones_valores = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Acciones2")
-clima_valores = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Clima2")
-
-start_year = 1979
-end_year = 1982
-
-output_path_WEAP = r"C:\Users\vagrant\Documents\WEAP_vagrant_multimachine\src\output\WEAP"
-output_path_MODFLOW = r"C:\Users\vagrant\Documents\WEAP_vagrant_multimachine\src\output\MODFLOW"
-
-ZB = ['Zones.zbr', 'Zones_RL.zbr']
-zones = ['P01','P02','P03','P07','P08','L01','L02','L05','L06','L09','L10','L12']
-path_WEAP = r"C:\Users\vagrant\Documents\WEAP Areas\Ligua_Petorca_WEAP_MODFLOW_RDM"
-
-
-#### Inicializamos el modelo 
-weap_model = LP_WEAP(acciones, activaciones, clima, demanda, acciones_valores, 
-                    clima_valores, start_year, end_year, output_path_WEAP, output_path_MODFLOW, path_WEAP, ZB, zones)
-
-#### Generamos el data frame de futuros
-weap_model.build_future_id_df()
-
-#### Corremos el modelo
-weap_model.run_WEAP_model(155)
-
 if __name__ == "__main__":
+#### Cargamos datos
+    acciones = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Acciones")
+    activaciones = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Activacion")
+    clima = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Clima")
+    demanda = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Demanda")
+
+    acciones_valores = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Acciones2")
+    clima_valores = pd.read_excel("../datos/Characterization.xlsx",sheet_name="Clima2")
+
+    start_year = 1979
+    end_year = 1982
+
+    output_path_WEAP = r"C:\Users\vagrant\Documents\WEAP_vagrant_multimachine\src\output\WEAP"
+    output_path_MODFLOW = r"C:\Users\vagrant\Documents\WEAP_vagrant_multimachine\src\output\MODFLOW"
+
+    ZB = ['Zones.zbr', 'Zones_RL.zbr']
+    zones = ['P01','P02','P03','P07','P08','L01','L02','L05','L06','L09','L10','L12']
+    path_WEAP = r"C:\Users\vagrant\Documents\WEAP Areas\Ligua_Petorca_WEAP_MODFLOW_RDM"
+
+
+    #### Inicializamos el modelo 
+    weap_model = LP_WEAP(acciones, activaciones, clima, demanda, acciones_valores, 
+                        clima_valores, start_year, end_year, output_path_WEAP, output_path_MODFLOW, path_WEAP, ZB, zones)
+
+    #### Generamos el data frame de futuros
+    weap_model.build_future_id_df()
+
+    #### Corremos el modelo
+    weap_model.run_WEAP_model(155)
+
     weap_model.processing_MODFLOW()
     weap_model.post_processing_MODFLOW()
 
