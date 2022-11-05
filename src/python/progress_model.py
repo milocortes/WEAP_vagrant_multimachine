59b1efa10cfc675df00f02e3e142cdda2e1f36fb
milocortes <hermilocg@hotmail.com>
2022-11-05 12:56:52 -0600

    fix progess_model

--- src/python/progress_model.py
+++ src/python/progress_model.py
@@ -2,10 +2,12 @@ import glob
 import os
 
 
+all_lines = []
 with open('log_execution.txt') as f:
     for line in f:
-        pass
-    last_line = line
+      all_lines.append(line.replace("\n",""))
+
+all_lines = ",".join(all_lines)
 
 os.chdir(r"C:\Users\vagrant\Documents\WEAP Areas\Ligua_Petorca_WEAP_MODFLOW_RDM\NWT_RDM_v22")
 
@@ -30,5 +32,5 @@ for anio_init in [str(i) for i in anios_total]:
         acumula_asteriscos += " "
 acumula_asteriscos += "|" + f" {ultimo_anio}-{final_year} "  +str(round((cuenta_anios/len(anios_total))*100,2)) + "%"
 
-print(f"Run Id {last_line}")
+print(f"Run Id executed : {all_lines}")
 print(acumula_asteriscos)
